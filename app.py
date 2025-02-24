65b5b015afc932f5f6c9a82d94eaa20d81bf7831
l27001 <l27001@ezdomain.ru>
2025-02-24 22:51:59 +0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,98 @@
+import os
+import cv2
+import numpy as np
+from flask import Flask, request, render_template, send_file, jsonify
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+UPLOAD_FOLDER = "uploads"
+ALLOWED_EXTENSIONS = {"jpg", "jpeg", "png", "tiff", "bmp"}
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/resize/<filename>", methods=["POST"])
+def resize(filename):
+    data = request.form
+    try:
+        width = int(data.get("width"))
+        height = int(data.get("height"))
+        scale = float(data.get("scale").replace(',', '.'))
+    except Exception as e:
+        print(e)
+        return jsonify({"error": "Некорректные данные"}), 400
+    filepath = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+    if(width < 1 or height < 1 or scale < 0 or scale > 4):
+        return jsonify({"error": "Некорректные размеры"}), 400
+    if not os.path.exists(filepath):
+        return jsonify({"error": "Файл не найден"}), 404
+    try:
+        img = cv2.imread(filepath)
+        h, w = img.shape[:2]
+        
+        if(width == w and height == h and scale == 1):
+            return jsonify({"filename": f"{filename}", "filepath": filepath, "size": img.shape, "scale": scale})
+        elif scale != 0:
+            width = int(w * scale)
+            height = int(h * scale)
+        else:
+            scale = 0
+
+        # Автоматический выбор метода интерполяции
+        if scale and scale > 1:
+            interpolation = cv2.INTER_CUBIC
+        elif scale and scale < 1:
+            interpolation = cv2.INTER_AREA
+        else:
+            interpolation = cv2.INTER_LINEAR
+
+        resized_img = cv2.resize(img, (width, height), interpolation=interpolation)
+        resized_filepath = os.path.join(app.config["UPLOAD_FOLDER"], f"resized_{filename}")
+        cv2.imwrite(resized_filepath, resized_img)
+        
+        return jsonify({"filename": f"resized_{filename}", "filepath": resized_filepath, "size": resized_img.shape, "scale": scale})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+
+@app.route("/upload", methods=["POST"])
+def upload_file():
+    if "file" not in request.files or request.files["file"].filename == "":
+        return jsonify({"error": "Файл не выбран"}), 400
+    
+    file = request.files["file"]
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        filepath = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+        file.save(filepath)
+        
+        try:
+            img = cv2.imread(filepath)
+            if img is None:
+                os.remove(filepath)
+                return jsonify({"error": "Изображение повреждено"}), 400
+        except Exception as e:
+            os.remove(filepath)
+            return jsonify({"error": str(e)}), 400
+        
+        return jsonify({"filename": filename, "filepath": filepath, "size": img.shape})
+    else:
+        return jsonify({"error": "Неподдерживаемый тип изображения"}), 400
+
+@app.route("/preview/<filename>")
+def preview_image(filename):
+    filepath = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+    if os.path.exists(filepath):
+        return send_file(filepath, mimetype="image/jpeg")
+    return jsonify({"error": "Файл не найден"}), 404
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/script.js
@@ -0,0 +1,95 @@
+let Gdata, Ldata;
+function showMessage(type, message) {
+    let msgdiv = document.getElementById('messages');
+    msgdiv.insertAdjacentHTML('beforeend', `<div class="alert alert-${type} alert-dismissible fade show" role="alert">
+  ${message}
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>`);
+}
+
+async function downloadImage() {
+    const image = await fetch(document.getElementById('preview').src)
+    const imageBlog = await image.blob()
+    const imageURL = URL.createObjectURL(imageBlog)
+  
+    const link = document.createElement('a')
+    link.href = imageURL
+    link.download = 'image file name here'
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+}
+
+function submitChanges() {
+    updateImage(Ldata, 1);
+}
+
+function updateImage(data, new_=0) {
+    let img = document.getElementById('preview');
+    img.src = null;
+    img.src = `/preview/${data.filename}`;
+    img.style.display = 'block';
+    img.height = data.size[0];
+    img.width = data.size[1];
+    document.getElementById("resizeHeight").value = data.size[0];
+    document.getElementById("resizeWidth").value = data.size[1];
+    Ldata = data;
+    if(new_ == 1) {
+        document.getElementById("resizeScale").value = 1;
+        Gdata = data;
+    }
+}
+
+function reeset() {
+    updateImage(Gdata, 1);
+}
+
+function uploadFile() {
+    let fileInput = document.getElementById('imageLoader');
+    let file = fileInput.files[0];
+    if (!file) {
+        showMessage("danger", "Необходимо выбрать файл!");
+        return;
+    }
+    let formData = new FormData();
+    formData.append('file', file);
+
+    fetch('/upload', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.error) {
+            showMessage("danger", data.error);
+
+        } else {
+            updateImage(data, 1);
+            document.getElementById("accordionExample").hidden = 0;
+        }
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+function resize(hw=0) {
+    let formData = new FormData(document.getElementById("resizeForm"));
+    if(hw == 1) {
+        formData.set("scale", 0);
+    }
+
+    fetch('/resize/' + Gdata.filename, {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.error) {
+            showMessage("danger", data.error);
+
+        } else {
+            updateImage(data);
+            document.getElementById("resizeScale").value = data.scale;
+        }
+    })
+    .catch(error => console.error('Error:', error));
+}
